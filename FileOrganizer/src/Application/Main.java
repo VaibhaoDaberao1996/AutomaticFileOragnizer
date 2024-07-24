0ebd2dafcfa75f43e4ca1e1906221e0cc020c5f7
vaibhao daberao <vaibhaodaberaoofficial@gmail.com>
2024-07-24 16:07:12 +0530

    PH 1 -Reading file in given directory

--- FileOrganizer/src/Application/Main.java
+++ FileOrganizer/src/Application/Main.java
@@ -1,9 +1,26 @@
 package Application;
 
+import java.util.List;
+import java.util.Scanner;
+
+import FileOrganize.OrganizeFile;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Enter you path :");
+		String path = scan.nextLine();
+
+		OrganizeFile orgFile = new OrganizeFile();
+
+		List<String> names = orgFile.getName(path);
+		for (String name : names) {
+			System.out.println(name);
+		}
+
 	}
 
 }
